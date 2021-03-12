3eb66a7100c24aaf9c1385f0a1a0f00254dedbb4
Alexey Goncharov <x401om@gmail.com>
2021-03-12 20:27:25 +0300

    – upgrade with ios sdk version 1.12.4 – removed unnecessary event channel invocations for getPaywalls and updatePurchaserInfo methods – use tryParse instead of parse for DateTime parsing – updated README – version bump to 0.3.3

--- lib/helpers/extensions.dart
+++ lib/helpers/extensions.dart
@@ -1,5 +1,5 @@
 extension MapExtension on Map<String, dynamic> {
   DateTime dateTimeOrNull(String key) {
-    return this.containsKey(key) ? DateTime.parse(this[key]) : null;
+    return this.containsKey(key) ? DateTime.tryParse(this[key]) : null;
   }
 }
