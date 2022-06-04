1c40495fe77737fabb7d94141eae188791219498
Niyaz Fazlyev <>
2022-06-04 11:32:05 +0300

    Removes dependency on shared_preferences, uuid

--- example/lib/service.dart
+++ example/lib/service.dart
@@ -2,9 +2,7 @@ import 'dart:io';
 
 import 'package:adapty_flutter/adapty_flutter.dart';
 
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_apns/flutter_apns.dart';
-import 'package:uuid/uuid.dart';
 
 class Service {
   static final PushConnector? _connector = Platform.isIOS ? createPushConnector() : null;
@@ -28,15 +26,6 @@ class Service {
   }
 
   static Future<String> getOrCreateInstallId() async {
-    final prefs = await SharedPreferences.getInstance();
-    String installId;
-    if (prefs.containsKey('install_id')) {
-      installId = prefs.getString('install_id')!;
-    } else {
-      installId = Uuid().v4();
-      prefs.setString('install_id', installId);
-    }
-
-    return installId;
+    return Future.value('your-install-id');
   }
 }
