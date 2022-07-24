6398a924e2c6498b1d07b7c199559d08f052b519
vladg7 <vlguriev@mail.ru>
2022-07-24 20:47:47 +0400

    upgrade with android sdk version 1.11.0

--- android/src/main/kotlin/com/adapty/flutter/serialization/AdaptyErrorSerializer.kt
+++ android/src/main/kotlin/com/adapty/flutter/serialization/AdaptyErrorSerializer.kt
@@ -1,6 +1,7 @@
 package com.adapty.flutter.serialization
 
 import com.adapty.errors.AdaptyError
+import com.adapty.errors.AdaptyErrorCode
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonSerializationContext
@@ -20,8 +21,36 @@ class AdaptyErrorSerializer : JsonSerializer<AdaptyError> {
         context: JsonSerializationContext
     ): JsonElement {
         return JsonObject().apply {
-            addProperty(ADAPTY_CODE, src.adaptyErrorCode.value)
+            addProperty(ADAPTY_CODE, intFromErrorCode(src.adaptyErrorCode))
             addProperty(MESSAGE, src.message.orEmpty())
         }
+    }
+
+    private fun intFromErrorCode(code: AdaptyErrorCode): Int = when (code) {
+        AdaptyErrorCode.UNKNOWN -> 0
+        AdaptyErrorCode.USER_CANCELED -> 2
+        AdaptyErrorCode.ITEM_UNAVAILABLE -> 5
+        AdaptyErrorCode.ADAPTY_NOT_INITIALIZED -> 20
+        AdaptyErrorCode.PAYWALL_NOT_FOUND -> 21
+        AdaptyErrorCode.PRODUCT_NOT_FOUND -> 22
+        AdaptyErrorCode.INVALID_JSON -> 23
+        AdaptyErrorCode.CURRENT_SUBSCRIPTION_TO_UPDATE_NOT_FOUND_IN_HISTORY -> 24
+        AdaptyErrorCode.PENDING_PURCHASE -> 25
+        AdaptyErrorCode.BILLING_SERVICE_TIMEOUT -> 97
+        AdaptyErrorCode.FEATURE_NOT_SUPPORTED -> 98
+        AdaptyErrorCode.BILLING_SERVICE_DISCONNECTED -> 99
+        AdaptyErrorCode.BILLING_SERVICE_UNAVAILABLE -> 102
+        AdaptyErrorCode.BILLING_UNAVAILABLE -> 103
+        AdaptyErrorCode.DEVELOPER_ERROR -> 105
+        AdaptyErrorCode.BILLING_ERROR -> 106
+        AdaptyErrorCode.ITEM_ALREADY_OWNED -> 107
+        AdaptyErrorCode.ITEM_NOT_OWNED -> 108
+        AdaptyErrorCode.NO_PURCHASES_TO_RESTORE -> 1004
+        AdaptyErrorCode.FALLBACK_PAYWALLS_NOT_REQUIRED -> 1008
+        AdaptyErrorCode.AUTHENTICATION_ERROR -> 2002
+        AdaptyErrorCode.BAD_REQUEST -> 2003
+        AdaptyErrorCode.SERVER_ERROR -> 2004
+        AdaptyErrorCode.REQUEST_FAILED -> 2005
+        AdaptyErrorCode.MISSING_PARAMETER -> 2007
     }
 }
