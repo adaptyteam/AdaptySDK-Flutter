3ea7bf158c575b68bf36162748db8be792a2d0d7
Alexey Goncharov <x401om@gmail.com>
2022-12-12 15:46:16 +0100

    cleanup constants

--- ios/Classes/SwiftAdaptyFlutterConstants.swift
+++ ios/Classes/SwiftAdaptyFlutterConstants.swift
@@ -6,36 +6,20 @@
 //
 
 struct SwiftAdaptyFlutterConstants {
-    // Channel
     static let channelName = "flutter.adapty.com/adapty"
 
-    // Arguments
     static let value = "value"
-    static let params = "params"
-    static let userInfo = "user_info"
-
-    static let appKey = "api_key"
-    static let observerMode = "observer_mode"
-    static let customerUserId = "customer_user_id"
-
     static let id = "id"
-
     static let paywall = "paywall"
+    static let paywalls = "paywalls"
     static let fetchPolicy = "fetch_policy"
-
+    static let customerUserId = "customer_user_id"
+    static let params = "params"
     static let product = "product"
-    static let offerId = "offer_id"
-    static let variationId = "variation_id"
-    static let transactionId = "transaction_id"
-
-    static let receipt = "receipt"
-    static let paidAccessLevel = "paid_access_level"
     static let attribution = "attribution"
     static let source = "source"
     static let networkUserId = "network_user_id"
-
-    static let paywalls = "paywalls"
-    static let forceUpdate = "force_update"
-
     static let onboardingParams = "onboarding_params"
+    static let variationId = "variation_id"
+    static let transactionId = "transaction_id"
 }
