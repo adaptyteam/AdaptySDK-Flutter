698823dfabe5f68e8ba561824ce02010b34a3752
Aleksei Valiano <vallianos@gmail.com>
2024-11-14 17:46:13 +0100

    fix method id

--- lib/src/constants/method.dart
+++ lib/src/constants/method.dart
@@ -1,12 +1,11 @@
 class Method {
-  static const String activate = 'activate';
+  static const String activate = 'activate'; 
   static const String setLogLevel = 'set_log_level';
   static const String getProfile = 'get_profile';
   static const String updateProfile = 'update_profile';
   static const String identify = 'identify';
   static const String getPaywall = 'get_paywall';
   static const String getPaywallProducts = 'get_paywall_products';
-  static const String getProductsIntroductoryOfferEligibility = 'get_products_introductory_offer_eligibility';
   static const String makePurchase = 'make_purchase';
   static const String restorePurchases = 'restore_purchases';
   static const String updateAttribution = 'update_attribution';
@@ -16,6 +15,8 @@ class Method {
   static const String setFallbackPaywalls = 'set_fallback_paywalls';
   static const String logout = 'logout';
   static const String presentCodeRedemptionSheet = 'present_code_redemption_sheet';
+  static const String getSDKVersion = 'get_sdk_version';
+  static const String isActivated = 'is_activated';
 
   static const String activateUI = 'adapty_ui_activate';
   static const String createView = 'adapty_ui_create_view';
