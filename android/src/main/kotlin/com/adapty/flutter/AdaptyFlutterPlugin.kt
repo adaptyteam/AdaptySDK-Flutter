f79a130ec929827f0ccd23b292e05ae9d6e6b33b
vlad <vlad@vg.codes>
2025-02-19 15:21:07 +0400

    update plugin

--- android/src/main/kotlin/com/adapty/flutter/AdaptyFlutterPlugin.kt
+++ android/src/main/kotlin/com/adapty/flutter/AdaptyFlutterPlugin.kt
@@ -3,6 +3,7 @@ package com.adapty.flutter
 import android.content.Context
 import com.adapty.internal.crossplatform.CrossplatformHelper
 import com.adapty.utils.FileLocation
+import io.flutter.FlutterInjector
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -10,19 +11,11 @@ import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.PluginRegistry
-import io.flutter.view.FlutterMain.getLookupKeyForAsset
 
 class AdaptyFlutterPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
 
     companion object {
         private const val CHANNEL_NAME = "flutter.adapty.com/adapty"
-
-        fun registerWith(registrar: PluginRegistry.Registrar) {
-            val instance = AdaptyFlutterPlugin();
-            instance.crossplatformHelper.setActivity { registrar.activity() }
-            instance.onAttachedToEngine(registrar.context(), registrar.messenger())
-        }
     }
 
     private var channel: MethodChannel? = null
@@ -46,6 +39,8 @@ class AdaptyFlutterPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel?.setMethodCallHandler(null)
+        channel = null
+        crossplatformHelper.release()
     }
 
     override fun onDetachedFromActivity() {
@@ -73,7 +68,7 @@ class AdaptyFlutterPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
         CrossplatformHelper.init(
             context,
             { eventName, eventData -> channel?.invokeMethod(eventName, eventData) },
-            { value -> FileLocation.fromAsset(getLookupKeyForAsset(value)) },
+            { value -> FileLocation.fromAsset(FlutterInjector.instance().flutterLoader().getLookupKeyForAsset(value)) },
         )
     }
 
